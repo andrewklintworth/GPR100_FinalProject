fa9a01638c9cb6b206af1fd9d9f7a16d56ed1cfe
Andrew Klintworth <andrew.klintworth@mymail.champlain.edu>
2025-10-06 16:51:35 -0400

    Next

---
+++ Assets/PlayerPhysics.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPhysics : MonoBehaviour
+{
+    //public RigidBody;
+    public double horizontalSpeed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+        }
+    }
+}
