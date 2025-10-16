b17c936b350f56fff5256059464aae8008a73c9d
Andrew Klintworth <andrew.klintworth@mymail.champlain.edu>
2025-10-16 17:13:02 -0400

    New

--- Assets/PlayerPhysics.cs
+++ Assets/PlayerPhysics.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -12,6 +15,7 @@ public class PlayerPhysics : MonoBehaviour
     public int jumpHeight = 0;
     public float maxSpeed = 0;
     RaycastHit isGrounded;
+    bool hit=false;
 
 
     // Start is called before the first frame update
@@ -23,21 +27,25 @@ public class PlayerPhysics : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void FixedUpdate()
     {
+        
         // Right/Left movement
         if (Input.GetKey(KeyCode.A)){ PlayerRB.AddForce(new Vector3(-horizontalSpeed, 0, 0)); }
         if (Input.GetKey(KeyCode.D)){ PlayerRB.AddForce(new Vector3(horizontalSpeed, 0, 0)); }
         // Clamp velocity Right/Left/Up/Down
         PlayerRB.velocity=new Vector3(Mathf.Clamp(PlayerRB.velocity.x,-maxSpeed,maxSpeed),Mathf.Clamp(PlayerRB.velocity.y,-20,10000),0);
         // Jumping
-        Physics.SphereCast(transform.position, 1, Vector3.down, out isGrounded);
-        if (isGrounded.collider.name.Equals("Floor")&& Input.GetKey(KeyCode.W)) { PlayerRB.AddForce(new Vector3(0, jumpHeight, 0));}
-        
+        if (Physics.Raycast(transform.position, Vector3.down, out isGrounded, 2, 6)) { hit = true; } else { hit = false; }
 
+            Debug.Log(hit);
+        Debug.DrawRay(transform.position,Vector3.down,Color.red,1);
+        //Physics.SphereCast(transform.position, 1, Vector3.down, out isGrounded, 1, 6);
+        if (hit && Input.GetKey(KeyCode.W)) { PlayerRB.AddForce(new Vector3(0, jumpHeight, 0));}
+        
     }
 
 }
