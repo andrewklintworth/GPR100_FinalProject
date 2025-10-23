00289fb65ef32f727dc5ab654481e555083ec94a
Andrew Klintworth <andrew.klintworth@mymail.champlain.edu>
2025-10-23 16:00:25 -0400

    Player Physics

--- Assets/PlayerPhysics.cs
+++ Assets/PlayerPhysics.cs
@@ -14,6 +14,7 @@ public class PlayerPhysics : MonoBehaviour
     public float horizontalSpeed = 0;
     public int jumpHeight = 0;
     public float maxSpeed = 0;
+    int timerFixedUpdate = 10;
     RaycastHit isGrounded;
     bool hit=false;
 
@@ -32,19 +33,25 @@ public class PlayerPhysics : MonoBehaviour
 
     void FixedUpdate()
     {
-        
+        if (timerFixedUpdate > 0) { timerFixedUpdate--; }
+
         // Right/Left movement
         if (Input.GetKey(KeyCode.A)){ PlayerRB.AddForce(new Vector3(-horizontalSpeed, 0, 0)); }
         if (Input.GetKey(KeyCode.D)){ PlayerRB.AddForce(new Vector3(horizontalSpeed, 0, 0)); }
         // Clamp velocity Right/Left/Up/Down
         PlayerRB.velocity=new Vector3(Mathf.Clamp(PlayerRB.velocity.x,-maxSpeed,maxSpeed),Mathf.Clamp(PlayerRB.velocity.y,-20,10000),0);
         // Jumping
-        if (Physics.Raycast(transform.position, Vector3.down, out isGrounded, 2, 6)) { hit = true; } else { hit = false; }
+        RaycastHit hitinfo;
+        if (Physics.Raycast(transform.position, Vector3.down, out hitinfo, 0.6f)) {
+            if (hitinfo.collider.gameObject.layer == 6 && Input.GetKey(KeyCode.W)&&timerFixedUpdate==0)
+            {
+                timerFixedUpdate = 10;
+                PlayerRB.AddForce(new Vector3(0, jumpHeight, 0));
+                Debug.Log("I am colliding with the floor");
+            }
+        } 
 
-            Debug.Log(hit);
         Debug.DrawRay(transform.position,Vector3.down,Color.red,1);
-        //Physics.SphereCast(transform.position, 1, Vector3.down, out isGrounded, 1, 6);
-        if (hit && Input.GetKey(KeyCode.W)) { PlayerRB.AddForce(new Vector3(0, jumpHeight, 0));}
         
     }
 
