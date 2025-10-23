9ddd5eda8f82c2b5559575001d6133087ed81015
Andrew Klintworth <andrew.klintworth@mymail.champlain.edu>
2025-10-23 16:59:41 -0400

    More Player Physics

--- Assets/PlayerPhysics.cs
+++ Assets/PlayerPhysics.cs
@@ -35,14 +35,14 @@ public class PlayerPhysics : MonoBehaviour
     {
         if (timerFixedUpdate > 0) { timerFixedUpdate--; }
 
-        // Right/Left movement
-        if (Input.GetKey(KeyCode.A)){ PlayerRB.AddForce(new Vector3(-horizontalSpeed, 0, 0)); }
-        if (Input.GetKey(KeyCode.D)){ PlayerRB.AddForce(new Vector3(horizontalSpeed, 0, 0)); }
-        // Clamp velocity Right/Left/Up/Down
-        PlayerRB.velocity=new Vector3(Mathf.Clamp(PlayerRB.velocity.x,-maxSpeed,maxSpeed),Mathf.Clamp(PlayerRB.velocity.y,-20,10000),0);
         // Jumping
         RaycastHit hitinfo;
         if (Physics.Raycast(transform.position, Vector3.down, out hitinfo, 0.6f)) {
+            // Right/Left movement
+            if (Input.GetKey(KeyCode.A)) { PlayerRB.AddForce(new Vector3(-horizontalSpeed, 0, 0)); }
+            if (Input.GetKey(KeyCode.D)) { PlayerRB.AddForce(new Vector3(horizontalSpeed, 0, 0)); }
+            // Clamp velocity Right/Left/Up/Down
+            PlayerRB.velocity = new Vector3(Mathf.Clamp(PlayerRB.velocity.x, -maxSpeed, maxSpeed), Mathf.Clamp(PlayerRB.velocity.y, -20, 10000), 0);
             if (hitinfo.collider.gameObject.layer == 6 && Input.GetKey(KeyCode.W)&&timerFixedUpdate==0)
             {
                 timerFixedUpdate = 10;
