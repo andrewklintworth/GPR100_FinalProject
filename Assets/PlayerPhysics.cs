5bc887d916af5249ac667d541c0ab48bf5cc2348
Andrew Klintworth-Personal <andrew.klintworth@mymail.champlain.edu>
2025-10-28 17:01:52 -0400

    First Update
    First edit to the project while not in class

--- Assets/PlayerPhysics.cs
+++ Assets/PlayerPhysics.cs
@@ -14,9 +14,7 @@ public class PlayerPhysics : MonoBehaviour
     public float horizontalSpeed = 0;
     public int jumpHeight = 0;
     public float maxSpeed = 0;
-    int timerFixedUpdate = 10;
-    RaycastHit isGrounded;
-    bool hit=false;
+    int timerFixedUpdate = 2;
 
 
     // Start is called before the first frame update
@@ -35,23 +33,24 @@ public class PlayerPhysics : MonoBehaviour
     {
         if (timerFixedUpdate > 0) { timerFixedUpdate--; }
 
-        // Jumping
+      
         RaycastHit hitinfo;
-        if (Physics.Raycast(transform.position, Vector3.down, out hitinfo, 0.6f)) {
+        if (Physics.Raycast(transform.position, Vector3.down, out hitinfo, 1.6f)) {
             // Right/Left movement
             if (Input.GetKey(KeyCode.A)) { PlayerRB.AddForce(new Vector3(-horizontalSpeed, 0, 0)); }
             if (Input.GetKey(KeyCode.D)) { PlayerRB.AddForce(new Vector3(horizontalSpeed, 0, 0)); }
+
             // Clamp velocity Right/Left/Up/Down
             PlayerRB.velocity = new Vector3(Mathf.Clamp(PlayerRB.velocity.x, -maxSpeed, maxSpeed), Mathf.Clamp(PlayerRB.velocity.y, -20, 10000), 0);
+
+            // Jumping
             if (hitinfo.collider.gameObject.layer == 6 && Input.GetKey(KeyCode.W)&&timerFixedUpdate==0)
             {
-                timerFixedUpdate = 10;
+                timerFixedUpdate = 2;
                 PlayerRB.AddForce(new Vector3(0, jumpHeight, 0));
-                Debug.Log("I am colliding with the floor");
             }
         } 
 
-        Debug.DrawRay(transform.position,Vector3.down,Color.red,1);
         
     }
 
